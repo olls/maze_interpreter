88839c3c0c1d78428ffea35bd5c529c36abb74f5
grit96 <geraintwhite@gmail.com>
2013-06-23 20:25:46 +0100

    Modified my maze

--- interpreter.py
+++ interpreter.py
@@ -488,7 +488,7 @@ def main():
     i = 0
     while maze.running:
         i += 1
-        if maze_out and i > 0:
+        if maze_out and i > 300:
             print(('\n'*80) + str(maze))
             time.sleep(1/FPS)
         maze.frame()
